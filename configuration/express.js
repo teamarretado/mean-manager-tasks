a07bc0b9b68cd49cb036238e3ad4f6bc7eaab8df
Hileo Andersson <hileomsi@gmail.com>
2016-09-10 10:45:50 -0300

    consign

--- configuration/express.js
+++ configuration/express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const consign = require('consign'); 
 
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,5 +12,11 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+consign({ cwd: 'api' })
+  .include('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
 
 module.exports = app;
